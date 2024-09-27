a0ea601ffa53e30e8705bdae60b906c0d7cdcd73
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-09-27 16:28:14 -0500

    Add SurveyResultActivity and refactor MainActivity
    - Replaced vote counters in MainActivity with a SurveyResultActivity view
    - Refactored MainActivity to send vote tallies to SurveyResultActivity with an Intent
    - Implemented vote tally reset functionality in SurveyResultActivity

--- app/src/main/java/com/example/yesnoquiz/MainActivity.kt
+++ app/src/main/java/com/example/yesnoquiz/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.yesnoquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 
+private const val TAG = "MainActivity"
+const val EXTRA_YES_RESULT = "com.example.yesnoquiz.YES_RESULT"
+const val EXTRA_NO_RESULT = "com.example.yesnoquiz.NO_RESULT"
+const val EXTRA_MUST_RESET = "com.example.yesnoquiz.MUST_RESET"
+
 class MainActivity : AppCompatActivity() {
+
+    private val surveyResultResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result -> handleActivityResult(result)
+    }
 
     private val voteTallyViewModel: VoteTallyViewModel by lazy {
         ViewModelProvider(this).get(VoteTallyViewModel::class.java)
@@ -17,9 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit private var yesButton: Button
     lateinit private var noButton: Button
-    lateinit private var yesVotesText: TextView
-    lateinit private var noVotesText: TextView
-    lateinit private var resetButton: Button
+    lateinit private var resultButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,30 +43,41 @@ class MainActivity : AppCompatActivity() {
 
         yesButton = findViewById(R.id.yes_button)
         noButton = findViewById(R.id.no_button)
-        yesVotesText = findViewById(R.id.yes_votes_text)
-        noVotesText = findViewById(R.id.no_votes_text)
-        resetButton = findViewById(R.id.reset_button)
-
-        fun updateVoteTotals() {
-            yesVotesText.text = voteTallyViewModel.getYesVotes().toString()
-            noVotesText.text = voteTallyViewModel.getNoVotes().toString()
-        }
+        resultButton = findViewById(R.id.result_button)
 
         yesButton.setOnClickListener {
             voteTallyViewModel.incrementYesVotes()
-            updateVoteTotals()
+            Log.d(TAG, "New yes count: ${voteTallyViewModel.getYesVotes()}")
         }
 
         noButton.setOnClickListener {
             voteTallyViewModel.incrementNoVotes()
-            updateVoteTotals()
+            Log.d(TAG, "New no count: ${voteTallyViewModel.getNoVotes()}")
         }
 
-        resetButton.setOnClickListener {
-            voteTallyViewModel.resetVotes()
-            updateVoteTotals()
+        resultButton.setOnClickListener {
+            showResults()
         }
+    }
 
-        updateVoteTotals()
+    private fun showResults() {
+        Intent(this, SurveyResultActivity::class.java).apply {
+            // "sending" the yes and no vote tallies over to SurveyResultActivity
+            putExtra(EXTRA_YES_RESULT, voteTallyViewModel.getYesVotes())
+            putExtra(EXTRA_NO_RESULT, voteTallyViewModel.getNoVotes())
+            surveyResultResultLauncher.launch(this)
+        }
+    }
+
+    private fun handleActivityResult(result: ActivityResult) {
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val mustReset = intent?.getBooleanExtra(EXTRA_MUST_RESET, false) ?: false
+            if (mustReset) {
+                // if the user pressed the Reset button in SurveyResultActivity, the vote tallies
+                // will be reset here
+                voteTallyViewModel.resetVotes()
+            }
+        }
     }
 }
